0b1342265fc4fc464fb012a383bc8276e933002c
Diarmuid O Connor <doconnor@wit.ie>
2022-03-10 09:48:14 +0000

    Fix backward/foeward arrows

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -1,13 +1,14 @@
-import React, { FunctionComponent} from "react";
+import React, { FunctionComponent } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
-import { MovieT } from '../..//types'
- 
+import { MovieT } from "../..//types";
+import { useNavigate } from "react-router-dom";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
@@ -21,12 +22,17 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const MovieHeader : FunctionComponent<{movie: MovieT}> = ({ movie } ) => {
+const MovieHeader: FunctionComponent<{ movie: MovieT }> = ({ movie }) => {
   const classes = useStyles();
+  const navigate = useNavigate();
 
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton aria-label="go back" size="large">
+      <IconButton
+        aria-label="go back"
+        size="large"
+        onClick={() => navigate(-1)}
+      >
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
@@ -38,7 +44,11 @@ const MovieHeader : FunctionComponent<{movie: MovieT}> = ({ movie } ) => {
         <br />
         <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
       </Typography>
-      <IconButton aria-label="go forward" size="large">
+      <IconButton
+        aria-label="go forward"
+        size="large"
+        onClick={() => navigate(-1)}
+      >
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
